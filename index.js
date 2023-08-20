2f5d470d12a18a7a82583e94837eb4bd102e06c3
KSHITIZ <85351591+imkshitiz@users.noreply.github.com>
2023-08-20 17:25:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,87 @@
+let score = JSON.parse(localStorage.getItem
+    ('score')) || {
+        wins: 0,
+        losses: 0,
+        ties: 0
+    };
+
+     updateScore();
+
+    function playGame(playerMove){
+    const computerMove= pickComputerMove();
+
+    let result ='';
+        if(playerMove === 'Scissors'){
+            if(computerMove === 'rock'){
+                result = 'You Lose!';
+            }
+            else if(computerMove == 'Paper'){
+                result= 'You win!';
+            } else if(computerMove === 'Scissors'){
+                result = 'Tie!';
+            }
+        }
+
+        else if(playerMove === 'paper'){
+            if(computerMove === 'rock'){
+                result = 'You Win!';
+            }
+            else if(computerMove == 'Paper'){
+                result= 'Tie!';
+            } else if(computerMove === 'Scissors'){
+                result = 'You Lose!';
+            }
+        }
+
+        else if(playerMove==='rock'){
+            if(computerMove === 'rock'){
+        result='Tie!';
+        } else if(computerMove === 'Paper'){
+        result = 'You Lose!';
+        }
+         else if(computerMove === 'Scissors'){
+        result = 'You Win!'
+        }
+
+       }
+       if(result ==='You Win!'){
+        score.wins+=1;
+       }
+       else if(result === 'You Lose!'){
+        score.Losses+=1;
+       }
+       else if(result === 'Tie!'){
+        score.Ties+=1;
+       }
+
+    localStorage.setItem('score',JSON.stringify(score));
+    updateScore();
+    document.querySelector('.js-result').innerHTML = result;
+    document.querySelector('.js-moves').innerHTML
+     = `You   
+     You
+        <img src="./images/${playerMove}-emoji.png"
+        class="move-icon">
+        <img src="./images/${computerMove}-emoji.png"
+        class="move-icon">
+        Computer`;
+    
+    }
+
+   function updateScore(){
+    document.querySelector('.js-score').innerHTML = `Wins: ${score.wins}, Losses: ${score.Losses}, Ties: ${score.Ties}`;
+   }
+
+    function pickComputerMove(){
+     const randomNumber = Math.random();
+    let computerMove ='';
+    if(randomNumber >= 0 && randomNumber < 1/3){
+         computerMove='rock';
+    } else if (randomNumber >= 1/3 && randomNumber < 2/3){
+         computerMove='Paper';
+    }          
+    else if(randomNumber >= 2/3 && randomNumber < 1){
+        computerMove ='Scissors';
+    }
+    return computerMove;
+ }
